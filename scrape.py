fc916ba156f4fb98d3f7b58f41ec8f6b88958406
Anan Methasate <anan_m@me.com>
2019-10-14 03:28:48 +0700

    upload code

--- scrape.py
+++ scrape.py
@@ -1,2 +1,15 @@
-from rendertron.decorators.django import rendertron_render
+import requests
+import os
 
+tid = 30000000
+SERVER_BASE_URL = '0.0.0.0'
+PORT = '3000'
+MODE = 'render'
+
+r = requests.get(f'http://{SERVER_BASE_URL}:{PORT}/{MODE}/http://www.pantip.com/topic/{tid}')
+
+print('status code returned',r.status_code)
+
+with open(os.path.join('html_storage',f'{tid}.html'),'w') as f:
+    #save file
+    f.writelines(r.text)
