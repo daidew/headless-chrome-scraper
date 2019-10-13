6d5bf9b993647d52ee9724d2e169bc97bbde2d42
Anan Methasate <anan_m@me.com>
2019-10-14 05:31:43 +0700

    scrape is now functional, not sure if logger works

--- scrape.py
+++ scrape.py
@@ -1,15 +1,74 @@
 import requests
 import os
+from multiprocessing import Pool, TimeoutError
+import time
+import argparse
 
-tid = 30000000
+start_tid,end_tid = 30000000,30000001
 SERVER_BASE_URL = '0.0.0.0'
 PORT = '3000'
 MODE = 'render'
+verbose = False
+overwrite = False
+k = 4
+visited = set()
 
-r = requests.get(f'http://{SERVER_BASE_URL}:{PORT}/{MODE}/http://www.pantip.com/topic/{tid}')
+def scrape(tid):
+    if tid in visited:
+        if overwrite:
+            if verbose:
+                print(f'overwriting {tid}')
+        else:
+            if verbose:
+                print(f'skipping {tid} as it alredy exists')
+            return
 
-print('status code returned',r.status_code)
+    r = requests.get(f'http://{SERVER_BASE_URL}:{PORT}/{MODE}/http://www.pantip.com/topic/{tid}')
+    ERROR_CODE = r.status_code
+    if verbose:
+        print(f'{tid}: status code returned',ERROR_CODE)
+    if ERROR_CODE != 200:
+        with open('log.txt','a') as f2: #not tested !!
+            f2.write(f'{tid}: got weird status code return {ERROR_CODE}\n')
+    with open(os.path.join('html_storage',f'{tid}.html'),'w') as f:
+        #save file
+        f.writelines(r.text)
 
-with open(os.path.join('html_storage',f'{tid}.html'),'w') as f:
-    #save file
-    f.writelines(r.text)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Scrape the Pantip website!')
+    parser.add_argument('range', nargs=2, type=int, help='scrape the website given tid range [a,b]')
+    parser.add_argument('-o','--overwrite', help='enable overwrite all existing storage files')
+    parser.add_argument('-a','--all', help='scrap all tids (approx 9 mil.)')
+    parser.add_argument('-v','--verbose', help='enable verbosity (expressive mode)')
+    parser.add_argument('-w','--workers', default=10, help='number of workers (multiprocessing)')
+
+    args = parser.parse_args()
+    k = args.workers
+    if(args.overwrite):
+        overwrite = True
+    if(args.all):
+        end_tid = 39315577
+    else:
+        start_tid, end_tid = args.range[0], args.range[1]+1
+    if args.verbose:
+        verbose = True
+        print('verbose mode ON')
+        print('worker set to',k)
+        if(args.overwrite):
+            print('overwrite mode ON')
+        if(args.all):
+            print('scraping ALL tids (this may take some time)')
+    try:
+        os.listdir('html_storage')
+        if verbose:
+            print('Found html_storage directory.')
+
+        visited = set([int(s.split('.')[0]) for s in os.listdir('html_storage')])
+    except:
+        print('html_storage directory not found. Creating a new directory now')
+        os.mkdir('html_storage')
+
+    pool = Pool(processes=k) # start k worker processes
+    pool.map(scrape, range(start_tid, end_tid)) #compute first k tids
